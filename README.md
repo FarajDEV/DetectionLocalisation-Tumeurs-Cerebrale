a7c2791ce3f6eba1550d5ac71ac071fdeb5d4f79
CHENIKI Faraj <chenikifaraj@hotmail.com>
2024-06-26 08:57:56 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# DetectionLocalisation-Tumeurs-Cerebrale
+ 𝙋𝙍𝙊𝙅𝙀𝙏 𝙉°2: Détection et localisation des tumeurs cérébrales.
---
+++ dataset/utilities.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:aeb164b5795b0d69a7638a3ec35c63a9adf246a00b4192e297eea673f5818b26
+size 6466
